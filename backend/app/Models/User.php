a79d44836298e8194e9bd26253c3e5b4737d34bf
emekajoseph <josephiwuji@gmail.com>
2026-01-24 12:34:37 +0100

    feat: establish initial Laravel user authentication backend and frontend API client.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable, HasUuids;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
